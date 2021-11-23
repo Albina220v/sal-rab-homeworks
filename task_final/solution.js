4433bba430fe09449d1b24105940f08a4d40bff4
Albina Ivanova <Albinasan220v@gmail.com>
2021-11-23 23:14:55 +0300

    finish - 1

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,18 +34,25 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+
+    //let data = {goods: [], order: {}};
+    let data = {client: {name, phone}, order: {address, sum}, goods: [{title, count}]};
 
     let countOfGoods = goods.length;
-
+    //let countOfGoods = goods
     for (let i = 0; i <= countOfGoods; i += 1) {
+    //for (let i = 0; i <= goods.length; i += 1) {
         data.goods.push(goods[i].title);
+        //countOfGoods(goods[i]);
     }
 
     data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
 
-    data.client = 'Иван';
+    //data.order.sum = name + phone + address + goods + sum;
+    data.order.sum = sum;
+
+    //data.client = 'Иван';
+    data.client.name = name;
 
     let jsonData = JSON.stringify(data);
 
